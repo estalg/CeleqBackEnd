70937844b02a5d1067e067bb2d015342c31e7633
Jorge <jorgea>
2019-09-26 15:24:36 -0600

    arreglo base datos

--- src/entities/entity.py
+++ src/entities/entity.py
@@ -2,17 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-'''
 db_url = '10.90.85.116'
 db_name = 'celeq_data_base'
 db_user = 'admin'
 db_password = 'Adminsql$celeq'
-'''
-db_url = 'localhost:3306'
-db_name = 'celeq_data_base'
-db_user = 'root'
-db_password = 'admin'
-engine = create_engine(f'mysql://{db_user}:{db_password}@{db_url}/{db_name}')
+
+engine = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_url}/{db_name}')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
